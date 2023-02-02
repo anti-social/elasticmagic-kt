22f5b09e588026567da9e9c44acc343b8463d5f5
a.usenko <a.usenko@smartweb.com.ua>
2023-02-02 16:11:59 +0200

    add test

---
+++ integ-tests/src/commonTest/kotlin/dev/evo/elasticmagic/PingTests.kt
@@ -0,0 +1,15 @@
+package dev.evo.elasticmagic
+
+import io.kotest.matchers.shouldBe
+import kotlin.test.Test
+
+
+class PingTests : ElasticsearchTestBase() {
+    override val indexName = "ping"
+
+    @Test
+    fun successfulPing() = runTestWithTransports {
+        val ping = this.cluster.ping()
+        ping.statusCode shouldBe 200
+    }
+}
