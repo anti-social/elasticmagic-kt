ec951bcce515926b374a8321e1a5df0f836b24ab
a.usenko <a.usenko@smartweb.com.ua>
2023-04-19 17:12:49 +0300

    add simple filter

---
+++ elasticmagic-query-filters/src/commonTest/kotlin/dev/evo/elasticmagic/qf/SimpleFilterTests.kt
@@ -0,0 +1,66 @@
+package dev.evo.elasticmagic.qf
+
+import dev.evo.elasticmagic.SearchQuery
+import dev.evo.elasticmagic.compile.BaseCompilerTest
+import dev.evo.elasticmagic.compile.SearchQueryCompiler
+import io.kotest.matchers.maps.shouldContainExactly
+import kotlin.test.Test
+
+class SimpleFilterTests : BaseCompilerTest<SearchQueryCompiler>(::SearchQueryCompiler) {
+
+
+    object BikeQueryFilters : QueryFilters() {
+        val price by SimpleFilter(BikeDocument.price, mode = FilterMode.INTERSECT)
+        val priceUnion by SimpleFilter(BikeDocument.price, mode = FilterMode.UNION)
+    }
+
+    @Test
+    fun intersectFilter() = testWithCompiler {
+
+        val sq = SearchQuery()
+        BikeQueryFilters.apply(sq, mapOf(listOf("price") to listOf("1.0", "3.0")))
+
+        compile(sq).body shouldContainExactly mapOf(
+            "query" to mapOf(
+                "bool" to mapOf(
+                    "filter" to listOf(
+                        mapOf(
+                            "bool" to mapOf(
+                                "filter" to listOf(
+                                    mapOf(
+                                        "term" to mapOf("price" to 1.0F)
+                                    ),
+                                    mapOf(
+                                        "term" to mapOf("price" to 3.0F)
+                                    )
+                                )
+                            ),
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    @Test
+    fun unionFilter() = testWithCompiler {
+
+        val sq = SearchQuery()
+
+        compile(sq).body shouldContainExactly mapOf()
+
+        BikeQueryFilters.apply(sq, mapOf(listOf("priceUnion") to listOf("2500", "2020")))
+
+        compile(sq).body shouldContainExactly mapOf(
+            "query" to mapOf(
+                "bool" to mapOf(
+                    "filter" to listOf(
+                        mapOf(
+                            "terms" to mapOf("price" to listOf(2500.0F, 2020.0F))
+                        )
+                    ),
+                ),
+            )
+        )
+    }
+}
---
+++ elasticmagic-query-filters/src/commonTest/kotlin/dev/evo/elasticmagic/qf/SimpleQueryFilterTests.kt
@@ -0,0 +1,80 @@
+package dev.evo.elasticmagic.qf
+
+import dev.evo.elasticmagic.SearchQuery
+import dev.evo.elasticmagic.compile.BaseCompilerTest
+import dev.evo.elasticmagic.compile.SearchQueryCompiler
+import io.kotest.matchers.maps.shouldContainExactly
+import kotlin.test.Test
+
+class SimpleQueryFilterTests : BaseCompilerTest<SearchQueryCompiler>(::SearchQueryCompiler) {
+
+
+    object BikeQueryFilters : QueryFilters() {
+        val price by SimpleQueryFilter(
+            "price",
+            values = listOf(
+                SimpleQueryValue("new", BikeDocument.price.gte(1000F)),
+                SimpleQueryValue("used", BikeDocument.price.lte(2000F))
+            ),
+            mode = FilterMode.INTERSECT
+        )
+        val priceUnion by SimpleQueryFilter(
+            "priceUnion",
+            values = listOf(
+                SimpleQueryValue("new", BikeDocument.price.gte(4000F)),
+                SimpleQueryValue("used", BikeDocument.price.lte(5000F))
+            ),
+            mode = FilterMode.UNION
+        )
+    }
+
+    @Test
+    fun intersectFilter() = testWithCompiler {
+
+        val sq = SearchQuery()
+        BikeQueryFilters.apply(sq, mapOf(listOf("price") to listOf("new", "used")))
+
+        compile(sq).body shouldContainExactly mapOf(
+            "query" to mapOf(
+                "bool" to mapOf(
+                    "filter" to listOf(
+                        mapOf(
+                            "bool" to mapOf(
+                                "must" to listOf(
+                                    mapOf("range" to mapOf("price" to mapOf("gte" to 1000.0F))),
+                                    mapOf("range" to mapOf("price" to mapOf("lte" to 2000.0F)))
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    @Test
+    fun unionFilter() = testWithCompiler {
+        val sq = SearchQuery()
+
+        compile(sq).body shouldContainExactly mapOf()
+
+        BikeQueryFilters.apply(sq, mapOf(listOf("priceUnion") to listOf("new", "used")))
+
+        compile(sq).body shouldContainExactly mapOf(
+            "query" to mapOf(
+                "bool" to mapOf(
+                    "filter" to listOf(
+                        mapOf(
+                            "bool" to mapOf(
+                                "should" to listOf(
+                                    mapOf("range" to mapOf("price" to mapOf("gte" to 4000.0F))),
+                                    mapOf("range" to mapOf("price" to mapOf("lte" to 5000.0F)))
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    }
+}
