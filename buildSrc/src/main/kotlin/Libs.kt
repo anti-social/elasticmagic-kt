c82061c094bf0f39f5469f9378dadc1a4628511f
a.usenko <a.usenko@smartweb.com.ua>
2024-10-16 14:03:28 +0300

    Update mkdocs

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -12,7 +12,7 @@ object Versions {
     const val grgit = "4.1.1"
     const val detekt = "1.22.0"
     const val dokka = "1.7.20"
-    const val mkdocs = "3.0.0"
+    const val mkdocs = "4.0.1"
     // With 0.13.0 ':integ-tests:transformJvmTestAtomicfu' task somehow fails
     const val binaryCompatibilityValidator = "0.12.1"
 
